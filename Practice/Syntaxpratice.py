8c2224bb45c68f2972212be997bf87a41c4ecf59
ompatil21 <officialompatil@gmail.com>
2024-11-02 16:28:45 +1100

    syntax practice

---
+++ Practice/Syntaxpratice.py
@@ -0,0 +1,11 @@
+# take input from user
+a = int(input("Enter First Number:-"))
+b = int(input("Enter Second Number:-"))
+c = int(input("Enter Thired number:-"))
+
+average = (a + b + c) / 3
+print(average)
+
+# multiline input using map
+o, m, p = map(int, input().split())
+print(o, m, p)
