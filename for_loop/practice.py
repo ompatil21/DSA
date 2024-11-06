922da0563479f9768570464aab9ef23de0cdbaf0
ompatil21 <officialompatil@gmail.com>
2024-11-06 13:58:44 +1100

    new questions solved

--- for_loop/practice.py
+++ for_loop/practice.py
@@ -26,12 +26,12 @@ def factorial(n):
 print(factorial(5))
 
 
-def pattern(n1):
+def pattern1(n1):
     for i in range(1, 8):
         print(i * 4, end=" ")
 
 
-pattern(4)
+pattern1(4)
 
 
 num1 = int(input("Enter a number for pattern printing: "))
@@ -39,3 +39,51 @@ for i in range(1, num1, 10):
     num = 1
     num = i * 10 + num
     print(num)
+
+
+"""2 22 222 2222 22222 pattern"""
+
+
+def pattern(n: int) -> None:
+    i: int = 1
+    num: int = 2
+    while i <= n:
+        print(num, end=" ")
+        num = (num * 10) + 2
+        i += 1
+    print()
+
+
+pattern(5)
+
+
+"""harmonic series pattern"""
+
+
+def harmonicseries(n):
+    if n < 1:
+        print("please enter a positive number")
+
+    total = 0.0
+    i = 1
+    while i <= n:
+        total += 1 / i
+        i += 1
+    return total
+
+
+print(harmonicseries(5))
+
+
+def pattern3(x, n):
+    i = 1
+    total = 0
+    y = 1
+    while i <= n:
+        total += x / y
+        y += 2
+        i += 1
+    return total
+
+
+print(pattern3(6, 4))
