b5a459a6f08c9744947b6d1df3a0767ee281dcc7
ompatil21 <officialompatil@gmail.com>
2024-11-11 13:32:45 +1100

    advance patterns

--- Nested_loops/Advance_patterns.py
+++ Nested_loops/Advance_patterns.py
@@ -56,3 +56,55 @@ for i in range(1, 6):
 
 
 print()
+
+
+"""
+n is given by user
+        1
+      2 2 2
+    3 3 3 3 3
+  4 4 4 4 4 4 4
+5 5 5 5 5 5 5 5 5
+"""
+
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print(i, end=" ")
+    print()
+
+
+print()
+
+
+"""
+o is given by user
+
+        1
+      2 2 2
+    3 3 3 3 3
+  4 4 4 4 4 4 4
+5 5 5 5 5 5 5 5 5
+  4 4 4 4 4 4 4
+    3 3 3 3 3
+      2 2 2
+        1
+
+"""
+o = 9
+
+for i in range(1, o // 2 + 2):
+    for j in range(1, o // 2 - i + 2):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print(i, end=" ")
+    print()
+for i in range(o // 2, 0, -1):
+    for j in range(1, o // 2 - i + 2):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print(i, end=" ")
+    print()
