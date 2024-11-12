3eb88760b581d8178c11f58ca1cad2e2ba95f381
ompatil21 <officialompatil@gmail.com>
2024-11-12 12:26:14 +1100

    advance patterns

--- Nested_loops/Advance_patterns.py
+++ Nested_loops/Advance_patterns.py
@@ -108,3 +108,93 @@ for i in range(o // 2, 0, -1):
     for k in range(1, 2 * i):
         print(i, end=" ")
     print()
+
+
+print()
+""""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+rows = 5
+"""
+# Number of rows for the pattern
+rows = 5
+
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+for i in range(rows - 1, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+print()
+"""
+5
+4 5
+3 4 5
+2 3 4 5
+1 2 3 4 5
+2 3 4 5
+3 4 5
+4 5
+5
+"""
+# Number of rows in the pattern
+n = 5
+
+# Upper part of the pattern
+for i in range(n, 0, -1):
+    for j in range(i, n + 1):
+        print(j, end=" ")
+    print()
+
+# Lower part of the pattern
+for i in range(2, n + 1):
+    for j in range(i, n + 1):
+        print(j, end=" ")
+    print()
+
+
+print()
+
+"""
+    1
+   1 2 3
+  1 2 3 4 5
+ 1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+ 1 2 3 4 5 6 7
+  1 2 3 4 5
+   1 2 3
+    1
+"""
+
+# Number of rows for the top half of the diamond
+n = 5
+
+# Upper part of the diamond
+for i in range(1, n + 1):
+    # Print leading spaces
+    print(" " * (n - i), end="")
+    # Print increasing sequence
+    for j in range(1, 2 * i):
+        print(j, end=" ")
+    print()
+
+# Lower part of the diamond
+for i in range(n - 1, 0, -1):
+    # Print leading spaces
+    print(" " * (n - i), end="")
+    # Print decreasing sequence
+    for j in range(1, 2 * i):
+        print(j, end=" ")
+    print()
