d67d5d7fc30a673fb0406f893ef8981b70472b0c
ompatil21 <officialompatil@gmail.com>
2025-03-25 13:53:49 +1100

    insertion sort

---
+++ sorting/insertion.py
@@ -0,0 +1,25 @@
+"""
+The task is to complete the insertsort() function which is used to implement Insertion Sort.
+
+Examples:
+
+Input: arr[] = [4, 1, 3, 9, 7]
+Output: [1, 3, 4, 7, 9]
+Explanation: The sorted array will be [1, 3, 4, 7, 9].
+Input: arr[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Explanation: The sorted array will be [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+Input: arr[] = [4, 1, 9]
+Output: [1, 4, 9]
+Explanation: The sorted array will be [1, 4, 9].
+"""
+
+
+def insertionSort(self, arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = key
